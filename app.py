73332a3da025e0272a151c9831133358b0887830
kferchichi-lab <k.ferchichi@tpr.com.tn>
2026-06-23 15:29:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,7 @@ SOUS_EQUIPEMENTS = {
 
 # --- SIDEBAR ---
 with st.sidebar:
-    col_logo, col_titre = st.columns([1, 5])
-    with col_logo:
         st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6q1BtDSDgVnJZFo0hOBfQJoDS6OYiub-qfQ&s", width=150)
-    with col_titre:
         st.markdown("## Tunisie Profilés d'Aluminium")
         st.markdown("#### Direction Maintenance et Travaux Neufs")
     st.divider()
